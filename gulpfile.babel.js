43a602dce55600802fdaa6254ad7ba873d7d7ac7
ppby <salswnss@gmail.com>
2020-05-08 21:46:01 +0900

    #6.0 Deploying to GH Pages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,6 +6,9 @@ import image from "gulp-image";
 import sass from "gulp-sass";
 import autoprefixer from "gulp-autoprefixer";
 import miniCSS from "gulp-csso";
+import bro from "gulp-bro";
+import babelify from "babelify";
+import ghpages from "gulp-gh-pages";
 
 sass.compiler = require("node-sass");
 
@@ -24,12 +27,17 @@ const routes = {
     src: "src/scss/style.scss",
     dest: "build/css",
   },
+  js: {
+    watch: "src/js/**/*.js",
+    src: "src/js/main.js", // 다른 건 import해 오니까 하나만 쓴다.
+    dest: "build/js",
+  },
 };
 
 const pug = () =>
   gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
-const clean = () => del(["build/"]);
+const clean = () => del(["build/", ".publish"]);
 
 const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, open: true }));
@@ -54,12 +62,30 @@ const watch = () => {
   gulp.watch(routes.pug.watch, pug);
   gulp.watch(routes.img.src, img);
   gulp.watch(routes.scss.watch, styles);
+  gulp.watch(routes.js.watch, js); // 모든 js파일을 지켜본다.
 };
+
+const js = () =>
+  gulp
+    .src(routes.js.src)
+    .pipe(
+      bro({
+        transform: [
+          babelify.configure({ presets: ["@babel/preset-env"] }),
+          ["uglifyify", { global: true }],
+        ],
+      })
+    )
+    .pipe(gulp.dest(routes.js.dest));
+
+const gh = () => gulp.src("build/**/*").pipe(ghpages());
 
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug, styles]);
+const assets = gulp.series([pug, styles, js]);
 
 const live = gulp.parallel([webserver, watch]);
 
-export const dev = gulp.series([prepare, assets, live]);
+export const build = gulp.series([prepare, assets]);
+export const dev = gulp.series([build, live]);
+export const deploy = gulp.series([build, gh, clean]);
